1de16c875d674503a9533b8d75d8f913dd90735c
Loid Padre <loidpadre@MacBook-Air-de-Loid.local>
2025-07-09 00:11:28 -0300

    Initial commit - Kumoxi frontend base

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1 className="m-4">Kumoxi esta vivo 🛰️</h1>
+       <Button variant="outline">Destructive</Button>
+
+    </div>
+  )
+}
+
+export default App
